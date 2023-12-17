507f8ce77e72095d4e19ee6487dfbb05f1f89cf9
Abdullah-Masto <abdullahmasto77@gmail.com>
2023-12-17 22:42:03 +0300

    add the project

---
+++ count.py
@@ -0,0 +1,30 @@
+import cv2
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+# import matplotlib.pyplot as plt
+
+def count(object:str,image_path:str):
+    # Load the image
+    image = cv2.imread(image_path)
+
+    # Detect common objects in the image
+    boxes, labels, confidences = cv.detect_common_objects(image,0.5,0.3,'yolov3')
+
+    # Draw bounding boxes on the image
+    output = draw_bbox(image, boxes, labels, confidences)
+    output_rgb = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
+    cv2.imwrite('./images/output.png',output_rgb)
+
+    # # Display the images
+    # plt.figure(figsize=(10, 10))
+    # plt.axis('off')
+    # plt.imshow(output_rgb)
+    # plt.show()
+
+    num_cars = labels.count(object)
+    return (f"Number of {object}: {num_cars}",output_rgb)
+
+if __name__ == '__main__':
+    test = count('car','./images/cars.png')
+    print(test[0])
+    cv2.imwrite('./images/output.png',test[1])
---
+++ main.py
@@ -0,0 +1,74 @@
+from PyQt5.QtCore import Qt
+from count import count
+import sys
+from PyQt5.QtWidgets import QApplication , QMainWindow , QFileDialog , QPushButton , QLabel , QTextEdit, QWidget 
+from PyQt5.QtGui import QDragEnterEvent, QDragMoveEvent, QDropEvent, QPixmap , QIcon
+from PyQt5.uic import loadUi
+
+class MainWindow(QMainWindow):
+    def __init__(self) :
+        super().__init__()
+        loadUi('design.ui',self)
+        self.setWindowIcon(QIcon('./images/countIcon.png'))
+        self.browseBtn:QPushButton
+        self.browseBtn.clicked.connect(self.browse_files)
+        self.countBtn:QPushButton
+        self.countBtn.clicked.connect(self.count_action)
+
+    def count_action(self):
+        self.oNameText:QTextEdit
+        self.numObjectsLabel:QLabel
+        obj_name=self.oNameText.toPlainText()
+        if obj_name == '':
+            return
+        if self.image_path == None:
+            self.numObjectsLabel.setText("please choose an image!")
+        else:
+            result=count(obj_name,self.image_path)
+            self.numObjectsLabel.setText(result[0])
+
+    def browse_files(self):
+        fname=QFileDialog.getOpenFileName(self,'Select Image','E:\qt_programs\count\images','Images (*.png *.xmp *.jpg *.jpeg *.svg)')
+        if(fname[0]==''):
+            return
+        self.imgLabel:QLabel
+        self.set_image(fname[0])
+
+        
+        
+    def dragEnterEvent(self, event: QDragEnterEvent | None) :
+        if event.mimeData().hasImage:
+            event.accept()
+        else:
+            event.ignore()
+
+    def dragMoveEvent(self, event: QDragMoveEvent | None) :
+        if event.mimeData().hasImage:
+            event.accept()
+        else:
+            event.ignore()
+    
+    def dropEvent(self, event: QDropEvent | None) :
+        if event.mimeData().hasImage:
+            event.setDropAction(Qt.CopyAction)
+            file_path=event.mimeData().urls()[0].toLocalFile()
+            self.set_image(file_path)
+            event.accept()
+        else:
+            event.ignore()
+
+    def set_image(self,file_path):
+        self.image_path=file_path
+        pixmap = QPixmap(file_path)
+        scaled_pixmap = pixmap.scaled(
+                self.imgLabel.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        self.imgLabel.setPixmap(scaled_pixmap)
+
+        
+
+app=QApplication([])
+mainWindow=MainWindow()
+mainWindow.setWindowTitle('Object Counter')
+mainWindow.show()
+app.exec_()
+
